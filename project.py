4921799c38dd34424ee44dfac51abcfc4f118131
m0rfey <ddiversantt@gmail.com>
2016-02-04 14:55:21 +0200

    Was changed encrypt

--- project.py
+++ project.py
@@ -4,36 +4,35 @@ import os
 
 symbol = (u'а', u'б', u'в', u'г', u'д', u'е', u'є', u'ж', u'з', u'и', u'і', u'ї', u'й', u'к', u'л', u'м',
           u'н', u'о', u'п', u'р', u'с', u'т', u'у', u'ф', u'х', u'ц', u'ч', u'ш', u'щ', u'ь', u'ю',
-          u'я', u' ', u',', u'.', u'!', u'?')
+          u'я', u' ', u',', u'.', u'!', u'?', u'\n')
 
 ks = 0
 
 sp = []
-f = open('xxx.txt')
+f = open('xxx_.txt')
 
 print('Виберіть варінт 1 або 2', '\n', u'1 - Кодувати', '\n',u'2 - Декодувати')
 number = input('Варіант: ')
 
 #encrypt
 if number == '1':
-    if os.stat("xxx.txt").st_size == 0:
+    if os.stat("xxx_.txt").st_size == 0:
         for iz in symbol:
             print(iz,':')
             s = input(u' ')
-            try:
-                if not s:
-                    print(u'Не коректно! Поля не повинні бути пустими')
-                    break
-                else:
-                    sp.append(s)
-                    print(sp)
-            except (ValueError, TypeError):
-                print(u'-')
 
-        f = open('xxx.txt', 'w')
+            if not s:
+                print(u'Не коректно! Поля не повинні бути пустими')
+                s = input(u' ')
+                break
+            else:
+                sp.append(s)
+                print(sp)
+
+        ff = open('xxx_.txt', 'w')
         for ix in sp:
-            f.write("%s\n" % ix)
-        f.close()
+            ff.write("%s\n" % ix)
+        ff.close()
 
     else:
         global spp
@@ -41,7 +40,7 @@ if number == '1':
         for ex in f.readlines():
             spp.append(ex)
 
-    txt = input(u'Введіть текст: \n')
+    txt = input(u'Введіть текст: \n').
     k = []
     for i in txt.lower():
         for z in range(0, len(symbol)):
@@ -56,6 +55,14 @@ if number == '1':
 elif number == '2':
 
     print(u'Декодування')
+
+    txt_dec = input(u'Введіть текст для декодування:\n')
+
+    if not txt_dec:
+        print('Error')
+    else:
+        print('Continue...')
+
 
 
 
